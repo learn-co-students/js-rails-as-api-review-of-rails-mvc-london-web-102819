8a863cdbaa21d05c07fcbb7e9ec5a4b937662f67
hiroshiuk1991 <hiroshis91@icloud.com>
2019-12-13 09:55:45 +0000

    Done.

--- db/migrate/20191213095057_create_birds.rb
+++ db/migrate/20191213095057_create_birds.rb
@@ -3,8 +3,6 @@ class CreateBirds < ActiveRecord::Migration[5.2]
     create_table :birds do |t|
       t.string :name
       t.string :species
- 
-      t.timestamps
     end
   end
-end ruby-2.6.1
+end
